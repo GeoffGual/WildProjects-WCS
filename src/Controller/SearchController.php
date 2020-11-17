8231d76ce5897909ab20a5681579dbe2bd8ca08d
Angelus-Dev-GitHub <t.dufosse666@gmail.com>
2020-11-17 15:27:45 +0100

    add message with error in return render

--- src/Controller/SearchController.php
+++ src/Controller/SearchController.php
@@ -5,28 +5,41 @@ namespace App\Controller;
 
 
 use App\Model\ProjectManager;
+use App\Service\FormValidator;
+use App\Service\ProjectValidator;
 use App\Service\SearchExist;
 
 class SearchController extends AbstractController
 {
     public function show()
     {
+        $errorMessages = [];
         if ($_SERVER['REQUEST_METHOD'] === 'POST') {
             if ($_POST['search'] == '') {
-                header('Location: /');
+                $formValidator = new ProjectValidator($_POST);
+                $formValidator->checkField();
+                $errorMessages = $formValidator->getErrors();
+                return $this->twig->render('Component/_navbar.html.twig', [
+                    'errors' => $errorMessages
+                ]);
             } else {
                 $words = $_POST['search'];
                 preg_match("/([^A-Za-z0-9\s])/", $words, $result);
                 if (empty($result)) {
-                    /*$words = explode(" ", $words);*/
                     $projectManager = new ProjectManager();
                     $projectsFound = $projectManager->selectByWordKey($words);
                     return $this->twig->render('Home/search.html.twig', [
                         'projectsFound' => $projectsFound,
                         'word' => $words,
+                        'errors' => $errorMessages
                     ]);
                 } else {
-                    header('Location: /');
+                    $formValidator = new FormValidator($_POST);
+                    $formValidator->addErrors('symbole', 'Pas de symbole dans la recherche');
+                    $errorMessages = $formValidator->getErrors();
+                    return $this->twig->render('Component/_navbar.html.twig', [
+                        'errors' => $errorMessages
+                    ]);
                 }
             }
         }
